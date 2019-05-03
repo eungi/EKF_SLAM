f3f9ea242a03a631145a4662b6fb0c79fb8ff1a4
eungi <jeg1221@naver.com>
2019-05-03 20:00:31 +0900

    main 만들기

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import rospy
+
 def main() :
 	print("EKF-SLAM Start")
 	# heading = degree
 
+	rospy.init_node('EKF-SLAM')
+	rate = rospy.Rate(2)	#20ms
+	while not rospy.is_shutdown():
+		#print('aa')
+		rate.sleep()
+
 if __name__ == '__main__':
-    main()
+	try :
+		main()
+	except rospy.ROSInterruptException :
+		pass
