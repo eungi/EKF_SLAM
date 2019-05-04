8fcaf6f1e3cffb66e4f882e220db0b8cb4152acd
eungi <jeg1221@naver.com>
2019-05-04 14:54:43 +0900

    topic sub

--- main.py
+++ main.py
@@ -2,16 +2,28 @@
 #-*- coding:utf-8 -*-
 
 import rospy
+import message_filters
+from mi_msgs.msg import Car
+from mi_msgs.msg import Car_LGIT
+
+def msg_callback(car_state, target_info):
+	#rospy.loginfo(car_state.velocity)
+	velocity = car_state.velocity
+	heading = car_state.heading
+	target1 = [target_info.targetPosX1, target_info.targetPosY1]
+	target2 = [target_info.targetPosX2, target_info.targetPosY2]
 
 def main() :
 	print("EKF-SLAM Start")
-	# heading = degree
 
-	rospy.init_node('EKF-SLAM')
-	rate = rospy.Rate(2)	#20ms
-	while not rospy.is_shutdown():
-		#print('aa')
-		rate.sleep()
+	rospy.init_node('EKF_SLAM')
+	car_messages = message_filters.Subscriber("car_messages", Car)
+	car_LGIT_messages = message_filters.Subscriber("car_LGIT_messages", Car_LGIT)
+
+	ts = message_filters.ApproximateTimeSynchronizer([car_messages, car_LGIT_messages], 1, 1, allow_headerless=True)
+	ts.registerCallback(msg_callback)
+
+	rospy.spin()
 
 if __name__ == '__main__':
 	try :
