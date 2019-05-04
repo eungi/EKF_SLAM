122edbc4f359d3b2c9c16aadb89c8847b3b543c7
eungi <jeg1221@naver.com>
2019-05-04 19:31:48 +0900

    vehicle 위치, 이동경로 visualization 완료

--- main.py
+++ main.py
@@ -3,15 +3,43 @@
 
 import rospy
 import message_filters
+from sensor_msgs.msg import Image
 from mi_msgs.msg import Car
 from mi_msgs.msg import Car_LGIT
 
+import visualization as vs
+from cv_bridge import CvBridge, CvBridgeError
+bridge = CvBridge()
+
+slam_pub = rospy.Publisher("SLAM", Image, queue_size=1)
+
+vehicle_poses = []
+target1_criterion = []
+target2_criterion = []
+
+def avm_changing_coordinate(criterion, pose) :
+	return (changed_x, changed_y)
+
 def msg_callback(car_state, target_info):
-	#rospy.loginfo(car_state.velocity)
+	#rospy.loginfo(car_state)
+	vehicle_pose = [car_state.PosX, car_state.PosY]
 	velocity = car_state.velocity
 	heading = car_state.heading
 	target1 = [target_info.targetPosX1, target_info.targetPosY1]
 	target2 = [target_info.targetPosX2, target_info.targetPosY2]
+	#print('target1 : ', target1, '       target2 : ', target2)
+
+	vehicle_poses.append(vehicle_pose)
+	if len(target1_criterion) is 0 and len(target2_criterion) is 0 :
+		target1_criterion.append(target1)
+		target2_criterion.append(target2)
+
+	t_state = vs.draw_t(vehicle_pose, heading, target1, target2)
+	vs.draw_path(t_state, vehicle_poses)
+
+	# Todo : Add EKF-SLAM
+
+	slam_pub.publish(bridge.cv2_to_imgmsg(t_state, "bgr8"))
 
 def main() :
 	print("EKF-SLAM Start")
---
+++ visualization.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import cv2
+import numpy as np
+
+HEIGHT = 400
+WIDTH = 400
+
+red = (0, 0, 255)
+green = (0, 255, 0)
+blue = (255, 0, 0)
+orange = (0, 155, 255)
+white = (255, 255, 255)
+
+def changing_visualization_coordinate(pose) :
+	changed_x = pose[0]*20*-1 + HEIGHT / 2
+	changed_y = pose[1]*20*-1 + WIDTH / 2
+
+	return (int(changed_x), int(changed_y))
+
+def draw_t(vehicle_pose, heading, target1, target2) :
+	heading = (heading + 180) * (3.141592 / 180)
+	heading_ = (heading + 90) * (3.141592 / 180)
+	basic_world = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+
+	# 원점 표시
+	cv2.line(basic_world, changing_visualization_coordinate((-0.5, 0)), changing_visualization_coordinate((0.5, 0)), white, 1)
+	cv2.line(basic_world, changing_visualization_coordinate((0, -0.5)), changing_visualization_coordinate((0, 0.5)), white, 1)
+
+	# 타겟 정보
+	cv2.circle(basic_world, changing_visualization_coordinate(target1), 3, red, -1)
+	cv2.circle(basic_world, changing_visualization_coordinate(target2), 3, orange, -1)
+
+	# 차량 정보
+	cv2.circle(basic_world, changing_visualization_coordinate(vehicle_pose), 3, blue, -1)
+	r = 4.5
+	end_point = ((-1*r*np.cos(heading))+vehicle_pose[0], (r*np.sin(heading)+vehicle_pose[1]))
+	cv2.line(basic_world, changing_visualization_coordinate(vehicle_pose), changing_visualization_coordinate(end_point), blue, 2)
+
+	return basic_world
+
+def draw_path(basic_world, poses) :
+	if len(poses) is 1 :
+		pass
+	else :
+		for i in range(len(poses) - 1) :
+			cv2.line(basic_world, changing_visualization_coordinate(poses[i]), changing_visualization_coordinate(poses[i+1]), green, 1)
